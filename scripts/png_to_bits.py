9189d822f0d4205056e739a267cf83e648a41574
awy2108 <awy2108@micro31.ee.columbia.edu>
2022-05-08 17:33:30 -0400

    Updates scripts to generate reverse order numbers

--- scripts/png_to_bits.py
+++ scripts/png_to_bits.py
@@ -10,7 +10,7 @@ SPRITES = {
 }
 
 if __name__ == "__main__":
-    im = Image.open('media/duckhunt_various_sheet.png').convert('RGB')
+    im = Image.open('media/duckhunt_various_sheet.png').convert('RGBA')
     pix = im.load()
 
     width, height = im.size
@@ -19,44 +19,57 @@ if __name__ == "__main__":
     print(im.size)
     print(pix[96,88])
     print(im.mode)
+    loader      = PackageLoader("scripts", "templates")
+    env         = Environment(loader=loader)
+    sprite_temp    = env.get_template('sprite.j2') 
+    color_temp     = env.get_template('colormap.j2')
 
     for sprite, coords in SPRITES.items():
         tx, ty, bx, by = coords[0], coords[1], coords[2], coords[3]
 
+        colorcount = defaultdict(lambda: 0)
+        for i in range(tx, bx):
+            for j in range(ty, by):
+                colorcount[pix[i,j]] += 1
+
+        max_colors = sorted([(v, k) for (k, v) in colorcount.items()])[::-1]
+        print(colorcount)
+        print(max_colors)
+        
+
+        out_colors = []
+        cm_ind = 1
+        for cnt, col in max_colors:
+            if col[3] == 0:
+                colorcount[col] = 0
+            else:
+                if cm_ind <= 3:
+                    out_colors.append(col)
+                colorcount[col] = min(cm_ind, 3)
+                cm_ind += 1
+
+        print(out_colors)
+        
+        # Write out color map
+        variables   = dict(id = 1, colors = out_colors)
+        output_from_parsed_template = color_temp.render(**variables)
+        print(output_from_parsed_template)
+
         for i in range(tx, bx, 16):
             for j in range(ty, by, 16):
-                block = [[0 for _ in range(16)] for _ in range(16)]
-
-                # find max 3 colors
-                colorcount = defaultdict(lambda: 0)
-                for p in range(i, min(i + 16, width)):
-                    for q in range(j, min(j + 16, height)):
-                        colorcount[pix[p,q]] += 1 
-
-                print(colorcount)
-
-                max_colors = sorted([(v, k, ind) for ind, (k, v) in enumerate(colorcount.items())])[::-1]
-                print(max_colors)
-                # TODO: write out color map
-
-                for cnt, col, ind in max_colors:
-                    colorcount[col] = min(ind + 1, 3)
-
                 # Generate hex numbers
-                nums = [] 
-                for p in range(i, min(i + 16, width)):
+                nums = ['0x0' for _ in range(16)] 
+                for q in range(j, min(j + 16, by)):
                     num = 0
-                    for q in range(j, min(j + 16, height)):
-                        num = num * 4 + colorcount[pix[p,q]]
-                    # print(hex(num))
-                    nums.append(str(hex(num)))
+                    for p in range(i, min(i + 16, bx)):
+                        # num = num * 4 + colorcount[pix[p,q]]
+                        num ^= (colorcount[pix[p,q]] << (2 * (p - i)))
+                    nums[q - j] = str(hex(num))
+                    # nums[q - j] = str(hex(rev))
 
                 print(nums)
                 
-                loader      = PackageLoader("scripts", "templates")
-                env         = Environment(loader=loader)
-                template    = env.get_template('sprite.j2') 
                 variables   = dict(id = 1, lines = nums)
-                output_from_parsed_template = template.render(**variables)
+                output_from_parsed_template = sprite_temp.render(**variables)
                 print(output_from_parsed_template)
 
